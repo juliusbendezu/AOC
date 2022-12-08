6a2df58d71e2429bdddecff0fbb9a98740efdb2b
Julius Bendezu <julius.bendezu@gmail.com>
2022-12-08 23:38:33 +0100

    Solutions for day 4

---
+++ 2022/04/2.js
@@ -0,0 +1,13 @@
+const { readArr } = require('./../../utils');
+
+exports.solution = () => {
+    const pairs = readArr('./input.txt').map(p => p.split(','));
+
+    let count = 0;
+    pairs.map(pair => pair.map(elf => elf.split('-').map(id => parseInt(id))))
+        .forEach(([[min1, max1], [min2, max2]]) => {
+            count += (min1 <= min2 && min2 <= max1) || (min2 <= min1 && min1 <= max2);
+        });
+
+    return count;
+}
