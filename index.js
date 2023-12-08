8ac59e0ffcbb1b358568b759b1973f7726c74b89
Julius Bendezu <julius.bendezu@gmail.com>
2023-12-08 14:31:41 +0100

    Arg to only run desired part of a day

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const fs = require("fs");
 require("dotenv").config();
 
-function runDay(year, day) {
+function runDay(year, day, part) {
   day = day.padStart(2, "0");
   const path = `${__dirname}/${year}/${day}`;
   process.chdir(path);
 
   const pt1 = require(`${path}/1`).solution;
   const pt2 = require(`${path}/2`).solution;
-  console.log(`Day ${day} part 1: ${pt1()}`);
-  console.log(`Day ${day} part 2: ${pt2()}`);
+  (!part || part == 1) && console.log(`Day ${day} part 1: ${pt1()}`);
+  (!part || part == 2) && console.log(`Day ${day} part 2: ${pt2()}`);
 }
 
 const args = process.argv.slice(2);
-const { YEAR, DAY } = process.env;
+const { YEAR, DAY, PART } = process.env;
 const year = args[0] || YEAR;
 const day = args[1] || DAY;
+const part = args[2] || PART;
 
 if (!year) {
   console.log(
-    "Not enough arguments passed to program. Either use 'npm start <year> (<day>)' or configure the .env file with desired year/day"
+    "Not enough arguments passed to program. Either use 'npm start <year> (<day>) (<part>)' or configure the .env file with desired year/day/part"
   );
   return;
 }
 
 console.log(`Year ${year}`);
 if (day) {
-  runDay(year, day);
+  runDay(year, day, part);
 } else {
   const days = fs.readdirSync(`./${year}`, { encoding: "utf-8" });
   days.forEach((d) => {
-    runDay(year, d);
+    runDay(year, d, part);
     console.log();
   });
 }
